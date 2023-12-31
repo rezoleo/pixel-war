27c334915161a556029955358b93a31a534cd173
Zaku <127139771+Zakurama@users.noreply.github.com>
2023-12-31 20:28:19 +0100

    added translation of canva

--- scripts/pixel_war.js
+++ scripts/pixel_war.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("pixel_war");
+var canvas = document.getElementById("pixel_war");
 const ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "green";
@@ -8,10 +8,10 @@ ctx.fillRect(0, 0, 100, 100);
 // Code for the button scaling the canvas
 
 var isScaled = false;
+canvas.style.transform = 'scale(4, 4)'
+canvas.style.transformOrigin = 'top center';
 
 document.getElementById('scaleButton').addEventListener('click', function() {
-    // Get the canvas element
-    var canvas = document.getElementById('pixel_war');
 
     // Toggle between scales
     if (isScaled) {
@@ -20,9 +20,51 @@ document.getElementById('scaleButton').addEventListener('click', function() {
         canvas.style.transform = 'scale(20, 20)';
     }
 
-    // Set the transform origin to "top center"
-    canvas.style.transformOrigin = 'top center';
-
     // Update the scale state
     isScaled = !isScaled;
+    finishMouseX = 0;
+    finishMouseY = 0;
+    translateX = 0;
+    translateY = 0;
+});
+
+// Event listeners for dragging
+var translateX = 0;
+var translateY = 0;
+var finishMouseX = 0;
+var finishMouseY = 0;
+var startMouseX = 0;
+var startMouseY = 0;
+var isDragging = false;
+
+document.addEventListener('mousedown', function (e) {
+    isDragging = true;
+    startMouseX = e.clientX;
+    startMouseY = e.clientY;
+});
+
+document.addEventListener('mousemove', function (e) {
+    if (isDragging) {
+        // Calculate the distance moved
+        var deltaX = e.clientX - startMouseX;
+        var deltaY = e.clientY - startMouseY;
+
+        translateX = deltaX + finishMouseX;
+        translateY = deltaY + finishMouseY;
+
+        // Update the canvas position
+        if (isScaled){
+            canvas.style.transform = 'translate(' + translateX + 'px, ' + translateY + 'px) scale(20, 20)';
+        } 
+        else {
+            canvas.style.transform = 'translate(' + translateX + 'px, ' + translateY + 'px) scale(4, 4)';
+        }
+    }
+
+});
+
+document.addEventListener('mouseup', function (e) {
+    isDragging = false;
+    finishMouseX += e.clientX - startMouseX;
+    finishMouseY += e.clientY - startMouseY;
 });
