c41d45a02ab4290134bd1d339d3ae81d44aa826d
Zaku <127139771+Zakurama@users.noreply.github.com>
2024-01-26 20:04:34 +0100

    writing pixel simplification

--- admin_login.php
+++ admin_login.php
@@ -18,4 +18,3 @@ if (isset($_POST['passwd'])) {
         exit();
     }
 }
-?>
--- write_pixel.php
+++ write_pixel.php
@@ -41,14 +41,15 @@ if (isset($data['x']) && isset($data['y']) && isset($data['color'])) {
         exit();
     }
 
-    // Transforme la couleur en entier en fonction de sa position dans la liste
+    // Transforme la couleur en entier en fonction de sa position dans la liste et l'utilise pour écrire le pixel dans le fichier
     $colorIndex = array_search($color, $colors);
-
-    // Utilise l'index de couleur pour faire les opérations nécessaires
+    
     $filename = 'private/pixels.bin';
-    $file = fopen($filename, 'r+b');
-    // Attention à la priorité des opérations
+
+    //calcul de l'offset
     $offset = (int)(($line * $width + $column) / 2);
+
+    $file = fopen($filename, 'r+b');
     fseek($file, $offset, SEEK_SET);
 
     $byte = fread($file, 1);
@@ -58,44 +59,15 @@ if (isset($data['x']) && isset($data['y']) && isset($data['color'])) {
     $byte = unpack("C", $byte);
     $byte = $byte[1];
 
-    //écriture du pixel dans le fichier en fonction du nombre de colonnes total (width) et de la colonne actuelle (column) (chaque pixel est codé sur 4 bits, donc 2 pixels par octet)
-    //si le nombre de colonnes total (width) est pair l'écriture sur les bits de poids ou faible se fait uniquement en fonction de la colonne actuelle (column)
+    //ecriture du pixel en fonction de sa parité en
 
-    if ($width % 2 == 0){
-        // Si la colonne est paire, on modifie les 4 premiers bits, sinon les 4 derniers
-        if ($column % 2 == 1) {
-            $byte = ($byte & 0xF0) | $colorIndex;
-        } else {
-            $byte = ($byte & 0x0F) | ($colorIndex << 4);
-        }
-
-        fwrite($file, pack("C", $byte));
+    if (($line * $width + $column) % 2 == 1) {
+        $byte = ($byte & 0xF0) | $colorIndex;
+    } else {
+        $byte = ($byte & 0x0F) | ($colorIndex << 4);
     }
-    else {
-        //tout dépend de la ligne si la ligne est paire c'est comme avant, sinon c'est l'inverse
-        if ($line % 2 == 0){
-            if ($column % 2 == 1) {
-                $byte = ($byte & 0xF0) | $colorIndex;
-            } else {
-                $byte = ($byte & 0x0F) | ($colorIndex << 4);
-            }
-
-            fwrite($file, pack("C", $byte));
-        }
-        else{
-            if ($column % 2 == 0) {
-                $byte = ($byte & 0xF0) | ($colorIndex << 4);
-            } else {
-                $byte = ($byte & 0x0F) | $colorIndex;
-            }
-
-            fwrite($file, pack("C", $byte));
-        
-        }
-    }
-
-    //fin de la partie écriture du pixel dans le fichier en fonction des différents cas
-
+    fwrite($file, pack("C", $byte));
+    
     fclose($file);
     echo "success";
     exit();
